30338509695be3ed5f2fdf2ae42d8235b1a7524f
dejoda <jojhitanarce@gmail.com>
2025-11-27 22:00:25 -0500

    Configuración añadida para GitHub Pages

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,17 +1,17 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import App from "../App";
 import Inicio from "../pages/inicio";
 import pagina404 from "../pages/pagina404";
 import Personajes from "../pages/Personajes";
 import Planetas from "../pages/Planetas";
 
-export const routes = createBrowserRouter(
+export const routes = createHashRouter(
   [
     {
-      path: "",
+      path: "/",
       Component: App,
       children: [
-        { path: "", Component: Inicio },
+        { path: "/", Component: Inicio },
         { path: "personajes", Component: Personajes },
         { path: "planetas", Component: Planetas },
         { path: "*", Component: pagina404 },
