35724f810e463c2e390b548de3edc9622d5a980e
dejoda <jojhitanarce@gmail.com>
2025-11-27 22:24:32 -0500

    Creacion del proyecto Apí DBZ Cards Holograficas

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,24 +1,17 @@
-import { createHashRouter } from "react-router";
+import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Inicio from "../pages/inicio";
 import pagina404 from "../pages/pagina404";
 import Personajes from "../pages/Personajes";
 import Planetas from "../pages/Planetas";
 
-export const routes = createHashRouter(
-  [
-    {
-      path: "/",
-      Component: App,
-      children: [
-        { path: "/", Component: Inicio },
-        { path: "/personajes", Component: Personajes },
-        { path: "/planetas", Component: Planetas },
-        { path: "*", Component: pagina404 },
-      ],
-    },
-  ],
-  {
-    basename: "/ApiDBZ", // 👈 AQUI EL FIX
-  }
+export const routes=createBrowserRouter([
+  {path:'',Component:App,children:[
+    {path:'',Component:Inicio},
+    {path:'personajes',Component:Personajes},
+    {path:'planetas',Component:Planetas},
+    {path:'*',Component:pagina404}
+  ],}
+  
+],
 );
